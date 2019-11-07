5a97867b37c59ea972e5e2b174e0c6b074166a41
Dakad <3106338+Dakad@users.noreply.github.com>
2019-11-07 18:26:07 +0100

    Add gallery Thumbnail model

--- sql/models.py
+++ sql/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, exists
 from sqlalchemy.orm import relationship, sessionmaker, ColumnProperty
-from sqlalchemy.schema import Column, ForeignKey, PrimaryKeyConstraint
+from sqlalchemy.schema import Column, ForeignKey, PrimaryKeyConstraint, UniqueConstraint
 from sqlalchemy.types import Boolean, DateTime, Float, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -15,11 +15,12 @@ class TimestampMixin(object):
     
 
 class GalleryInfo(Base, TimestampMixin):
-    __tablename__ ="gallery_info"
-    
-    g_id = Column(String, primary_key=True)
-    g_token = Column(String, primary_key=True)
-    g_hash = ColumnProperty(g_id + "_" + g_token)
+    __tablename__ ="gallery_infos"
+
+    g_hash = Column(String, primary_key=True)
+
+    g_id = Column(String, nullable=False)
+    g_token = Column(String, nullable=False)
 
     title = Column(String)
     title_jp = Column(String)
@@ -39,9 +40,31 @@ class GalleryInfo(Base, TimestampMixin):
     favorite_category= Column(String)
     my_rating = Column(Float)
 
+    thumbnail_link = relationship('GalleryThumbnail', backref="gallery_infos", uselist=False)
+
+
+    def __init__(self, **kwargs):
+        for (var_name, var_val) in kwargs.items():
+            setattr(self, var_name, var_val)
+        self.g_hash = GalleryInfo.generate_hash(
+            kwargs.get("g_id"), 
+            kwargs.get("g_token")
+        )
+
+    @staticmethod
+    def generate_hash(g_id, g_token, separator="_"):
+        return "{}{}{}".format(g_id, separator, g_token)
+
+
+class GalleryThumbnail(Base, TimestampMixin):
+    __tablename__ = "gallery_thumbnails"
+    
+    g_hash = Column(Integer, ForeignKey(GalleryInfo.g_hash), primary_key=True)
+    thumb_link = Column(String, nullable=False)
 
 
 
+    
 
 
 
@@ -51,9 +74,14 @@ class Main:
 
     @staticmethod
     def run():
+
         #   INSERT
         if not session.query(exists().where(GalleryInfo.g_hash == '123_abc456')).scalar():
-            g1 = GalleryInfo(title="[Hajime Isayama] Shingeki no Kyojin | Attack on Titan", title_jp="進撃の巨人", g_id="123", g_token="abc456")
+            g1_id = "123"
+            g1_token = "abc456"
+            # g1_hash = GalleryInfo.generate_hash(g1_id, g1_token)
+            g1 = GalleryInfo(title="[Hajime Isayama] Shingeki no Kyojin | Attack on Titan", title_jp="進撃の巨人", g_id="123", g_token=g1_token)
+            g1.posted_at = datetime(2016, 1, 1)
             g1.my_rating = 4.5
             g1.language = "English"
             g1.is_translated = True
@@ -84,10 +112,11 @@ if __name__ == '__main__':
     database_uri = 'sqlite:///tmp/db.sqlite3'
     engine = create_engine(database_uri, echo=False)
     
+    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
     connection = engine.connect()
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
     session = Session()
 
     Main.run()
